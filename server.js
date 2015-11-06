e8af02b6cec4f36f2559f8fa9992b8cadd5300eb
dxeware <dxeware@gmail.com>
2015-11-05 16:31:24 -0800

    Re-factor done and test done

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
+'use strict';
+
 var net = require('net');
 var fs = require('fs');
 
 function saveToFile(string, callback) {
 
+  // Set up directory
+  // Set up file name to be unique -- using timestamp string
   var time = new Date().toTimeString();
   var outputDir = __dirname + '/files/';
-  fileName = 'req.timestamp' + time.replace(/ /g, '');
+  var fileName = 'req.timestamp' + time.replace(/ /g, '');
 
   // If output directory doesn't exist, create
   if (!fs.existsSync(outputDir)){
-    fs.mkdirSync(outputDir,0744);
+    fs.mkdirSync(outputDir);
   }
 
+  // Write file with string as content
   fs.writeFile(outputDir+fileName, string, function(err) {
     if (err) throw err;
 
     console.log("File was saved!");
+
+    //Send file name back
     callback(fileName);
   });
 
@@ -23,15 +30,14 @@ function saveToFile(string, callback) {
 
 var server = net.createServer(function (socket) {
   socket.on('data', function(data) {
-    var fileName;
 
     console.log('==== Connection Started=====');
     console.log(data.toString());
 
+    // If data received, write sting to file
     saveToFile(data.toString(), function(file) {
-      fileName = file;
-      console.log("returned fileName = " + fileName);
-      socket.write(fileName);
+      // Send filename back to client
+      socket.write(file);
     });
 
   });
--- test/tcp.Spec.js
+++ test/tcp.Spec.js
@@ -6,7 +6,12 @@ var fs = require('fs');
 
 describe('TCP server', function() {
 
-  var path = __dirname + '/..' + '/files';
+  var path = __dirname + '/..' + '/files/';
+
+  // If output directory doesn't exist, create
+  if (!fs.existsSync(path)){
+    fs.mkdirSync(path);
+  }
 
   beforeEach(function() {
     fs.readdirSync(path).forEach(function(fileName) {
@@ -17,25 +22,26 @@ describe('TCP server', function() {
     });
   });
 
-  it('should connect to server', function(done) {
+  it('should connect to server and file should match contents and filename returned', function(done) {
+
+    var writeStr = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui ex iusto ut inventore alias, placeat, voluptatibus recusandae minima molestiae. Consequuntur repudiandae libero, a. Dolores assumenda repellendus nostrum esse, impedit earum!\n';
 
     var client = net.connect(3000, function() {
       console.log('connected to TCP server');
     });
-    client.write('Something');
+    client.write(writeStr);
 
-    //expect(array).to.deep.equal(["blue", 3, "green", 10, "red", 55]);
-    //expect(array.length).to.equal(6);
     client.on('data', function(data) {
       console.log(data.toString());
       client.end();
 
-      fs.readdir(path, function(err, files) {
-        console.log("files = " + files);
-        expect(files[0]).to.equal(data.toString());
-        done();
-      })
+      var files = fs.readdirSync(path);
 
+      var contents = fs.readFileSync(path + files[0]).toString();
+
+      expect(files[0]).to.equal(data.toString());
+      expect(contents).to.equal(writeStr);
+      done();
 
     });
   });
